f4523fd28121a4fc7fe0b917221ec45b120dc51e
Pouya <pouyamaleki85@gmail.com>
2026-07-06 19:44:02 +0330

    add getter method for the adjacency List

--- src/Model/DataStructure/Graph.cpp
+++ src/Model/DataStructure/Graph.cpp
@@ -17,6 +17,12 @@ void Graph::addEdge(char from, char to, int weight)
     adjList[to].push_back({from, weight}); // add an edge for the second node
 }
 
+// adhacency list getter method implementation
+const map<char, vector<pair<char, int>>> &Graph::getadjList() const
+{
+    return adjList;
+}
+
 // constructor implementation
 Graph::Graph()
 {
@@ -24,33 +30,33 @@ Graph::Graph()
     char nodeNames[19]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'J', 'K', 'W', 'M', 'O', 'P', 'R', 'S', 'Q', 'T', 'U', 'V'};
 
     // for loop to create the nodes
-    for(char i: nodeNames)
+    for (char i : nodeNames)
     {
         addNode(i);
     }
 
     // create the edges between the nodes
-    addEdge('A' , 'B' , 3);
-    addEdge('A' , 'F' , 6);
-    addEdge('B' , 'C' , 2);
-    addEdge('C' , 'D' , 5);
-    addEdge('D' , 'E' , 1);
-    addEdge('E' , 'G' , 3);
-    addEdge('G' , 'F' , 4);
-    addEdge('F' , 'J' , 6);
-    addEdge('J' , 'K' , 3);
-    addEdge('K' , 'W' , 2);
-    addEdge('W' , 'M' , 3);
-    addEdge('W' , 'G' , 5);
-    addEdge('M' , 'G' , 4);
-    addEdge('M' , 'S' , 1);
-    addEdge('S' , 'R' , 2);
-    addEdge('R' , 'P' , 1);
-    addEdge('P' , 'O' , 3);
-    addEdge('O' , 'Q' , 2);
-    addEdge('Q' , 'T' , 5);
-    addEdge('T' , 'R' , 4);
-    addEdge('T' , 'U' , 2);
-    addEdge('U' , 'V' , 3);
-    addEdge('Q' , 'V' , 5);
+    addEdge('A', 'B', 3);
+    addEdge('A', 'F', 6);
+    addEdge('B', 'C', 2);
+    addEdge('C', 'D', 5);
+    addEdge('D', 'E', 1);
+    addEdge('E', 'G', 3);
+    addEdge('G', 'F', 4);
+    addEdge('F', 'J', 6);
+    addEdge('J', 'K', 3);
+    addEdge('K', 'W', 2);
+    addEdge('W', 'M', 3);
+    addEdge('W', 'G', 5);
+    addEdge('M', 'G', 4);
+    addEdge('M', 'S', 1);
+    addEdge('S', 'R', 2);
+    addEdge('R', 'P', 1);
+    addEdge('P', 'O', 3);
+    addEdge('O', 'Q', 2);
+    addEdge('Q', 'T', 5);
+    addEdge('T', 'R', 4);
+    addEdge('T', 'U', 2);
+    addEdge('U', 'V', 3);
+    addEdge('Q', 'V', 5);
 }

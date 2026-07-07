178afbf714d45abb3402b72eead781f401fa54e3
Pouya <pouyamaleki85@gmail.com>
2026-07-07 21:27:06 +0330

    remove and change some lines in the A* header file

--- Include/Model/Algorithms/PathFinders/AStar.h
+++ Include/Model/Algorithms/PathFinders/AStar.h
@@ -4,13 +4,11 @@
 #include "Graph.h"
 #include <functional>
 #include <queue>
-#include <algorithm>
 #include <cmath>
 
 struct AstarResult; // a struct to store the finall result of A* algorithm
-using heuristicFunction = std::function<int(char , char)>; // define a new function type
-AstarResult AStar(const Graph& , char , char , char , heuristicFunction); // A* algorithm function
-int manhatanHeuristic(char , char); // a function to turn the graph into a network
-int heuristic (char , char); // a function to calculate heauristic euclidean distance 
+using heuristicFunction = std::function<int(char, char)>; // define a new function type
+AstarResult AStar(const Graph &, char, char, char, heuristicFunction); // A* algorithm function
+int heuristic(char, char); // a function to calculate heauristic euclidean distance
 
 #endif ASTAR_H

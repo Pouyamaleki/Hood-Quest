5dfae834430906a450f30a6e42e505c47fb729a7
Pouya <pouyamaleki85@gmail.com>
2026-07-09 00:34:05 +0330

    add variables needed to BFS algorithm implementation

--- src/Model/Algorithms/BFS.cpp
+++ src/Model/Algorithms/BFS.cpp
@@ -3,8 +3,19 @@
 // BFS algorithm implementation
 vector<char> BFS(const Graph &graph, char playerPosition, char wolfPosition)
 {
+    // number of nodes in the graph
+    int nodeNumber = 19;
 
+    // create a queue for breadth search
+    queue<char> pq;
+    // a vector to stop search the repeated nodes
+    vector<bool> visitedNode(nodeNumber, false);
+    // a vector to store the parents nodes
+    vector<char> parentsNode(nodeNumber, -1);
+
+    // add the wolf postion to the queue
+    pq.push(wolfPosition);
 }
 
 // wolf next postion implementation
-char wolfNextPosition(char , char);
+char wolfNextPosition(char, char);

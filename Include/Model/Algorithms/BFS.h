e67486613b8553f32135ab15783cb2e52996f36b
Pouya <pouyamaleki85@gmail.com>
2026-07-09 01:13:53 +0330

    add BFS algorithm implementation in BFS source file

--- Include/Model/Algorithms/BFS.h
+++ Include/Model/Algorithms/BFS.h
@@ -4,10 +4,12 @@
 #include "Graph.h"
 #include <queue>
 #include <algorithm>
+#include <exception>
+#include <iostream>
 
 // bfs function to find the best path for the wolf
 vector<char> BFS(const Graph &, char, char);
 // return the next postion that the wolf should go
 char wolfNextPosition(char , char);
 
-#endif BFS_H
+#endif
--- src/Model/Algorithms/BFS.cpp
+++ src/Model/Algorithms/BFS.cpp
@@ -3,18 +3,87 @@
 // BFS algorithm implementation
 vector<char> BFS(const Graph &graph, char playerPosition, char wolfPosition)
 {
-    // number of nodes in the graph
-    int nodeNumber = 19;
+    // get the adjacency list from the graph
+    const map<char, vector<pair<char, int>>> &adjList = graph.getAdjList();
 
     // create a queue for breadth search
     queue<char> pq;
     // a vector to stop search the repeated nodes
-    vector<bool> visitedNode(nodeNumber, false);
+    map<char, bool> visitedNodes;
     // a vector to store the parents nodes
-    vector<char> parentsNode(nodeNumber, -1);
+    map<char, char> parentNodes;
+
+    // a for loop to inizialize every node
+    for (const auto &pair : adjList)
+    {
+        visitedNodes[pair.first] = false;
+        parentNodes[pair.first] = '\0';
+    }
 
     // add the wolf postion to the queue
     pq.push(wolfPosition);
+    visitedNodes[wolfPosition] = true;
+
+    // the main loop of the BFS algorithm
+    while (!pq.empty())
+    {
+        // store the first node
+        char current = pq.front();
+        pq.pop(); // remove the first node from queue
+
+        // if the current had reach the player position
+        if (current == playerPosition)
+        {
+            // create a vector to store the path
+            vector<char> path;
+            char node = current;
+
+            // a while loop to add the right nodes to the path
+            while (node != '\0')
+            {
+                path.push_back(node);     // add the node to the path
+                node = parentNodes[node]; // change the value of node to it's parent
+            }
+
+            // reverse the path to get the right order
+            reverse(path.begin(), path.end());
+            return path;
+        }
+
+        // find current in adjacency list map
+        auto it = adjList.find(current);
+
+        // check if the current node is in the map
+        if (it != adjList.end())
+        {
+            // a for loop to find the neighbors
+            for (const auto &neighbor : it->second)
+            {
+                char neighborNode = neighbor.first;
+
+                // prevent the system to check already visited nodes
+                if (!visitedNodes[neighborNode])
+                {
+                    visitedNodes[neighborNode] = true;
+                    parentNodes[neighborNode] = current;
+                    pq.push(neighborNode);
+                }
+            }
+        }
+    }
+
+    // if there is not any possible path
+    try
+    {
+        throw "could not find any path for wolf movement";
+    }
+
+    catch (const char *x)
+    {
+        cout << x << endl;
+    }
+
+    return {};
 }
 
 // wolf next postion implementation

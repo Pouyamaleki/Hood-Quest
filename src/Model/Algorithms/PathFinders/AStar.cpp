43985b8aab67035550e57c5901fad394083239b7
Pouya <pouyamaleki85@gmail.com>
2026-07-08 23:48:41 +0330

    remove unnecessary lines from A* source file

--- src/Model/Algorithms/PathFinders/AStar.cpp
+++ src/Model/Algorithms/PathFinders/AStar.cpp
@@ -54,50 +54,15 @@ int heuristic(char from, char to)
     int dx = x1 - x2;
     int dy = y1 - y2;
 
+    // cast the sqrt in to a intiger
     return static_cast<int>(sqrt(dx * dx + dy * dy));
 }
 
 // A* algorithm function implementation
-AstarResult AStar(const Graph &graph, char from, char to, char wolfPosition)
+AstarResult AStar(const Graph &graph, char from, char to)
 {
-    // create a new graph to remove the wolf position
-    Graph newgraph;
-
-    // a for loop to create the nodes in filtered graph
-    for (const auto &node : graph.getAdjList())
-    {
-        if (node.first != wolfPosition)
-        {
-            newgraph.addNode(node.first);
-        }
-    }
-
-    // a for loop to copy the valid edges
-    for (const auto &node : graph.getAdjList())
-    {
-        char fromNode = node.first;
-        // skip the wolf position node
-        if (fromNode == wolfPosition)
-        {
-            continue;
-        }
-
-        // a for loop to navigate the edges of every node
-        for (const auto &edge : node.second)
-        {
-            char toNode = edge.first;
-            int weight = edge.second;
-
-            // skip the edges that connect to wolf position
-            if (toNode != wolfPosition)
-            {
-                newgraph.addEdge(fromNode, toNode, weight);
-            }
-        }
-    }
-
     // assign the adjacency list
-    const auto &adjList = newgraph.getAdjList();
+    const auto &adjList = graph.getAdjList();
 
     // check for the from and to nodes validation
     if (adjList.find(from) == adjList.end() || adjList.find(to) == adjList.end())

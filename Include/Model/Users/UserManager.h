7836c28bedc1f04367b1569a8c8b783f36b8a2de
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-08 13:14:56 +0330

    add a "usermanager class" & repurposing "user class"

---
+++ Include/Model/Users/UserManager.h
@@ -0,0 +1,19 @@
+#ifndef USERMANAGER_H
+#define USERMANAGER_H
+
+#include <iostream>
+#include <vector>
+#include "User.h"
+using namespace std;
+
+class Usermanager
+{
+private:
+    vector<User> Users;
+
+public:
+    void AddUser();
+    void DelUser();
+    int SearchUser();
+    void PrintScore();
+};

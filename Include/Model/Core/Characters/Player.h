5a37000a548600e9819e8cc262d573483fc3a49e
Pouya <pouyamaleki85@gmail.com>
2026-07-09 12:50:27 +0330

    complete player header file

--- Include/Model/Core/Characters/Player.h
+++ Include/Model/Core/Characters/Player.h
@@ -8,13 +8,16 @@ class player
 private:
     std::string playerName;
     char playerPosition;
+    int score;
 
 public:
-    player(std::string , char); // player class constructor
-    void playerSetter(std::string , char); // setter method to initialize the variables
-    void playerSetter(char); // overloaded seeter method
+    player(std::string, char); // player class constructor
+    void setPlayer(std::string, char, int); // setter method to initialize the variables
+    void setPosition(char); // setter method for player position
+    void setPosition(int); // setter method for player score
     std::string getName() const; // getter method for name variable
-    char getPlayerPosition() const; // getter method for player position variable
+    char getPosition() const; // getter method for player position variable
+    int getScore() const; // getter method for player score variable
 };
 
 #endif

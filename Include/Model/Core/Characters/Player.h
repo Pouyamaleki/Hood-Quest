00180b3d4ab2d6b3f32bc55db5647edf0e752abc
Pouya <pouyamaleki85@gmail.com>
2026-07-09 13:41:06 +0330

    complete player source file

--- Include/Model/Core/Characters/Player.h
+++ Include/Model/Core/Characters/Player.h
@@ -8,16 +8,18 @@ class player
 private:
     std::string playerName;
     char playerPosition;
-    int score;
+    int playerScore;
+
+    void setPlayer(std::string, char, int); // setter method to initialize the variables
+    void setName(std::string);              // setter method for player name
 
 public:
     player(std::string, char, int); // player class constructor
-    void setPlayer(std::string, char, int); // setter method to initialize the variables
-    void setPosition(char); // setter method for player position
-    void setPosition(int); // setter method for player score
-    std::string getName() const; // getter method for name variable
-    char getPosition() const; // getter method for player position variable
-    int getScore() const; // getter method for player score variable
+    void setPosition(char);         // setter method for player position
+    void setScore(int);             // setter method for player score
+    std::string getName() const;    // getter method for name variable
+    char getPosition() const;       // getter method for player position variable
+    int getScore() const;           // getter method for player score variable
 };
 
 #endif
--- src/Model/Core/Characters/Player.cpp
+++ src/Model/Core/Characters/Player.cpp
@@ -3,29 +3,49 @@
 // player constructor implementation
 player::player(std::string playerName, char playerPosition , int score)
 {
-
+    setPlayer(playerName , playerPosition , score);
 }
-void player::setPlayer(std::string, char, int)
+
+// set player method implementation
+void player::setPlayer(std::string name, char position, int score)
 {
-
+    setName(name);
+    setPosition(position);
+    setScore(score);
 }
-void player::setPosition(char)
+
+// name setter method implementation
+void player::setName(std::string name)
 {
-
+    playerName = name;
 }
-void player::setPosition(int)
+
+// position setter implememntation
+void player::setPosition(char Position)
 {
-
+    playerPosition = Position;
 }
+
+// score setter implementation
+void player::setScore(int score)
+{
+    playerScore = score;
+}
+
+// player name getter impleementation
 std::string player::getName() const
 {
-    
+    return playerName;   
 }
+
+// player position getter implementation
 char player::getPosition() const
 {
-
+    return playerPosition;
 }
+
+// player score getter implementation
 int player::getScore() const
 {
-
+    return playerScore;
 }

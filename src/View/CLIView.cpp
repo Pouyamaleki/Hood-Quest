f5f45b35f59d2e4474af785cb6bd15ef3bea4458
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-09 17:38:12 +0330

    develop "Cliview Class"

--- src/View/CLIView.cpp
+++ src/View/CLIView.cpp
@@ -1,1 +1,11 @@
 #include "Cliview.h"
+
+void Cliview::PrintMainMenu()
+{
+    cout << " ========== welcome to this game ==========\n";
+    cout << " please select an option:\n";
+    cout << " crate a new user: 1\n";
+    cout << " login: 2\n";
+    cout << " Show leaderboard: 3\n";
+    cout << " exit: 4\n";
+}

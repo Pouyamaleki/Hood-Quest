c535e2d36d369fd60f6e0ea9431f5c4cd94dd7f6
Pouya <pouyamaleki85@gmail.com>
2026-07-09 16:12:32 +0330

    update stack header file

--- Include/Model/DataStructure/Stack.h
+++ Include/Model/DataStructure/Stack.h
@@ -2,15 +2,16 @@
 #define STACK_H
 
 #include <vector>
+#include <utility>
 
 class Stack
 {
 private:
-    std:: vector <int> mystack; // create the vector
+    std::vector<std::pair<char, char>> mystack; // create the vector
 
 public:
     bool isEmpty() const; // a method to check if the stack is empty or no
-    void push(int); // a method to add things to the stack
+    void push(char, char); // a method to add things to the stack
     int pop(); // a method to remove the last thing that has been add to the stack
 };
 

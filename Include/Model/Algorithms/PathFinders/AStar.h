4d98e2e2c8ac7d2bbc75319285e0b4c991da3711
Pouya <pouyamaleki85@gmail.com>
2026-07-15 21:05:26 +0330

    add AStar getter method

--- Include/Model/Algorithms/PathFinders/AStar.h
+++ Include/Model/Algorithms/PathFinders/AStar.h
@@ -13,12 +13,13 @@ class AStar
 {
 private:
     vector<char> AStarpath;
-public:
-struct AstarResult; // a struct to store the finall result of A* algorithm
-AstarResult AStarAlgorithm(const Graph &, char, char, char); // A* algorithm function
-int heuristic(char, char); // a function to calculate heauristic euclidean distance
-void AStarprintPath(const Graph & , char , char , char);
-};
 
+public:
+    struct AstarResult; // a struct to store the finall result of A* algorithm
+    AstarResult AStarAlgorithm(const Graph &, char, char, char); // A* algorithm function
+    int heuristic(char, char); // a function to calculate heauristic euclidean distance
+    void AStarprintPath(const Graph &, char, char, char);// a method to print the suggested path
+    vector<char> getPath() const; // AStarPath getter method
+};
 
 #endif ASTAR_H

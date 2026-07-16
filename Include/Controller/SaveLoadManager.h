7197a894bf9ff670978f1fcb3c09cea0437b131b
Abbas Ashoury <abbasashoury84@gmail.com>
2026-07-16 18:05:48 +0330

    modify structure of "SaveLoadManager class"

--- Include/Controller/SaveLoadManager.h
+++ Include/Controller/SaveLoadManager.h
@@ -5,13 +5,9 @@
 #include "UserManager.h"
 using namespace std;
 
-class SaveLoadManager
-{
-private:
-public:
-    void save(const Usermanager &usermanager, const string &file);
-    void load(const Usermanager &usermanager, const string &file);
-    void Writh(const string &data, const string &file);
-    string read(const string &data, const string &file);
-};
+void save(const Usermanager &, const string &);
+void load(const Usermanager &, const string &);
+void Writh(const string &, const string &);
+string read(const string &, const string &);
+
 #endif SAVELOADMANAGER_H
--- src/Controller/SaveLoadManager.cpp
+++ src/Controller/SaveLoadManager.cpp
@@ -1,1 +1,14 @@
 #include "SaveLoadManager.h"
+
+void save(const Usermanager &usermanager, const string &file)
+{
+}
+void load(const Usermanager &usermanager, const string &file)
+{
+}
+void Writh(const string &data, const string &file)
+{
+}
+string read(const string &data, const string &file)
+{
+}

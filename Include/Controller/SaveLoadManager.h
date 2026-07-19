b2a9d9c90a4bbbd33b0d19d47d281b3d76e0de73
Pouya <pouyamaleki85@gmail.com>
2026-07-19 22:03:14 +0330

    add comment to Save Load Manager header file

--- Include/Controller/SaveLoadManager.h
+++ Include/Controller/SaveLoadManager.h
@@ -7,8 +7,8 @@
 #include "UserManager.h"
 using namespace std;
 
-bool fileChecker();
-void save(const Usermanager &, const string &);
-void load(Usermanager &, const string &);
+bool fileChecker(); // a function to check for the save.txt file
+void save(const Usermanager &, const string &); // a function to save the data in save.txt file
+void load(Usermanager &, const string &); // a function to load the data from save.txt file
 
 #endif

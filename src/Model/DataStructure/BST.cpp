b93b8ca2b1ec487ebf471efd8b2984efe08b0672
Pouya <pouyamaleki85@gmail.com>
2026-07-19 22:30:45 +0330

    add comments to the BST source file

--- src/Model/DataStructure/BST.cpp
+++ src/Model/DataStructure/BST.cpp
@@ -1,30 +1,37 @@
 #include "BST.h"
 
+// BSt construvtor implementation
 BST::BST()
 {
     root = nullptr;
 }
 
+// Bst destructor Implementation
 BST::~BST()
 {
     destroyHelper(root);
 }
 
+// destroy helper method implementation
 void BST::destroyHelper(BSTNode *node)
 {
     if (node == nullptr)
         return;
 
+    // destroy both left and right nodes
     destroyHelper(node->left);
     destroyHelper(node->right);
     delete node;
 }
 
+// insert helper method implementation
 BSTNode *BST::insertHelper(BSTNode *node, const string &username, long int score)
 {
+    // check if the node is not valid
     if (node == nullptr)
         return new BSTNode(username, score);
 
+    // add the node to the right place (Left or Roght)
     if (username < node->username)
         node->left = insertHelper(node->left, username, score);
     else if (username > node->username)
@@ -33,31 +40,42 @@ BSTNode *BST::insertHelper(BSTNode *node, const string &username, long int score
     return node;
 }
 
+// search helper method implementation
 BSTNode *BST::searchHelper(BSTNode *node, const string &username) const
 {
+    // check the node and the user validation
     if (node == nullptr || node->username == username)
         return node;
 
+    // search left node from the current node
     if (username < node->username)
         return searchHelper(node->left, username);
 
+    // search the right node from the current node
     return searchHelper(node->right, username);
 }
 
+// insert method implementation
 void BST::insert(const string &username, long int score)
 {
+    // start the search from the root to find the right place
     root = insertHelper(root, username, score);
 }
 
+// search method implementation
 BSTNode *BST::search(const string &username) const
 {
+    // start the search from the root
     return searchHelper(root, username);
 }
 
+// update score method implementation
 bool BST::updateScore(const string &username, long int newScore)
 {
+    // find the node
     BSTNode *node = search(username);
 
+    // if the node is not valid
     if (node == nullptr)
         return false;
 

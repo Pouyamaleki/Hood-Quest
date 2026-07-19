3ddcefb75471b1a545019565e5061ec8be60fc8f
Pouya <pouyamaleki85@gmail.com>
2026-07-19 22:17:39 +0330

    add comments to BST header file

--- Include/Model/DataStructure/BST.h
+++ Include/Model/DataStructure/BST.h
@@ -20,18 +20,18 @@ class BST
 private:
     BSTNode *root;
 
-    BSTNode *insertHelper(BSTNode *, const string &, long int);
-    BSTNode *searchHelper(BSTNode *, const string &) const;
+    BSTNode *insertHelper(BSTNode *, const string &, long int); // isert helper method for BST
+    BSTNode *searchHelper(BSTNode *, const string &) const; // search helper method for BST
     void destroyHelper(BSTNode *);
 
 public:
-    BST();
-    ~BST();
+    BST(); // BST constructor
+    ~BST(); // BST destructor
 
-    void insert(const string &, long int);
-    BSTNode *search(const string &) const;
-    bool updateScore(const string &, long int);
-    void clear();
+    void insert(const string &, long int); // insert method in BST
+    BSTNode *search(const string &) const; // search method in BST
+    bool updateScore(const string &, long int); // Update Score in BST
+    void clear(); // clear the BST
 };
 
 #endif
